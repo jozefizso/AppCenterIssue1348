64b06902cd6b41b2702c7a3667bb351d80a91c7e
Jozef Izso <jozef.izso@gmail.com>
2020-03-06 10:02:20 +0100

    Add AppCenter SDK

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
 
 namespace ConsoleApp2
 {
@@ -10,6 +12,11 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            AppCenter.Configure("secret");
+            AppCenter.Start(typeof(Analytics));
+
+
+            Console.ReadKey();
         }
     }
 }
